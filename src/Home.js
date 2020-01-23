8f3bdb6538b5e9acbde585b0334497f91bafdced
Leckie <hsk0094@gmail.com>
2020-01-23 15:48:48 +0900

    Getting data from the GraphQL API part One

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,21 @@
 import React from "react";
+import { Query } from "react-apollo";
+import { HOME_PAGE } from "./queries";
 
-const Home = () => "Home";
+const Home = () => (
+  <Query query={HOME_PAGE}>
+    {({ loading, error, data }) => {
+      const { movies } = data;
+      if (loading) return <p>Loading...</p>;
+      return (
+        <ul>
+          {movies.map((movie, idx) => (
+            <li key={idx}>{movie.title}</li>
+          ))}
+        </ul>
+      );
+    }}
+  </Query>
+);
 
 export default Home;
---
+++ src/queries.js
@@ -0,0 +1,11 @@
+import { gql } from "apollo-boost";
+
+export const HOME_PAGE = gql`
+  query {
+    movies(limit: 50, rating: 7) {
+      id
+      title
+      genres
+    }
+  }
+`;
